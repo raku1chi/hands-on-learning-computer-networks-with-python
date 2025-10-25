5df3d1d72be5558691e119f77b9a14c2837bff33
raku1chi <25150296+raku1chi@users.noreply.github.com>
2025-10-26 04:07:07 +0900

    実行例を更新

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from models import Link, NetworkGraph, Node, Packet
 
 
-def main():
+def ch01():
     network_graph = NetworkGraph()
 
     node1 = Node(node_id=1, address="00:01", network_graph=network_graph)
@@ -12,10 +12,37 @@ def main():
     print(node2)
     print(link)
 
-    packet = Packet(src="00:01", dst="00:02", payload="Hello, World!")
-    node1.send_packet(packet)
+    node1.send_packet(Packet(src="00:01", dst="00:02", payload="Hello, World!"))
+    node2.send_packet(Packet(src="00:02", dst="00:01", payload="Hi there!"))
+
+    for i in range(10):
+        node1.send_packet(
+            Packet(src="00:01", dst="00:02", payload=f"Packet {i} from Node 1")
+        )
+
+    node3 = Node(node_id=3, address="00:03", network_graph=network_graph)
+    Link(
+        node_x=node2,
+        node_y=node3,
+        bandwidth=1000000,
+        delay=0.01,
+        network_graph=network_graph,
+    )
+
+    node4 = Node(node_id=4, address="00:04", network_graph=network_graph)
+    Link(
+        node_x=node2,
+        node_y=node4,
+        bandwidth=100,
+        delay=0.1,
+        network_graph=network_graph,
+    )
 
     network_graph.draw()
+
+
+def main():
+    ch01()
 
 
 if __name__ == "__main__":
